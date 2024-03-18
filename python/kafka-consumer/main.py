ad5a3b1d13eabea647575be908223624c0c1fcbe
Chris O'Hara <cohara87@gmail.com>
2024-03-18 13:44:54 +1000

    Write messages to a webhook via Dispatch

--- python/kafka-consumer/main.py
+++ python/kafka-consumer/main.py
@@ -1,15 +1,49 @@
+import logging
+import threading
+
+import requests
+from fastapi import FastAPI
+from dispatch.fastapi import Dispatch
 from kafka import KafkaConsumer
 
-brokers = ["localhost:9092", "localhost:9093", "localhost:9094"]
-topic = "topic1"
-group_id = "group1"
 
-consumer = KafkaConsumer(topic, group_id=group_id, bootstrap_servers=brokers)
+logging.basicConfig(level=logging.WARNING)
 
-try:
-    for message in consumer:
-        print(message.value)
-except KeyboardInterrupt:
-    pass
-finally:
-    consumer.close()
+
+BROKERS = ["localhost:9092", "localhost:9093", "localhost:9094"]
+TOPIC = "topic1"
+GROUP = "group1"
+
+URL = "https://c8955422caa67b92b294cef222afe991.m.pipedream.net"
+
+
+app = FastAPI()
+dispatch = Dispatch(app)
+
+
+@dispatch.function
+def send_message(payload):
+    response = requests.post(URL, data=payload)
+    response.raise_for_status()
+    return resopnse
+
+
+def consume_messages():
+    consumer = KafkaConsumer(TOPIC, group_id=GROUP, bootstrap_servers=BROKERS)
+    try:
+        for message in consumer:
+            forward_batch([message])
+    finally:
+        consumer.close()
+
+
+def forward_batch(messages):
+    batch = dispatch.batch()
+    for message in messages:
+        batch.add(send_message, message.value)
+    batch.dispatch()
+
+
+# Consume messages in the background.
+thread = threading.Thread(target=consume_messages)
+thread.start()
