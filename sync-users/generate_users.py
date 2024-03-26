a4abe5891adcf5e85f0d8babdf808fbc1bec1e7a
Chris O'Hara <cohara87@gmail.com>
2024-03-26 11:22:15 +1000

    Write a script to populate a sqlite database with fake user info

---
+++ sync-users/generate_users.py
@@ -0,0 +1,57 @@
+import sqlite3
+from contextlib import closing
+from faker import Faker
+
+
+SEED = 0
+FILENAME = "users.db"
+COUNT = 100
+
+
+def generate_user(faker):
+    return {
+        "first_name": faker.first_name(),
+        "last_name": faker.last_name(),
+        "address": faker.address(),
+        "email": faker.email(),
+    }
+
+
+def populate_database(db, faker, count):
+    with closing(db.cursor()) as cursor:
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                first_name TEXT,
+                last_name TEXT,
+                address TEXT,
+                email TEXT
+            )
+        """
+        )
+
+        cursor.execute("DELETE FROM users")
+        cursor.execute("DELETE FROM sqlite_sequence WHERE name='users'")
+
+        for _ in range(count):
+            user = generate_user(faker)
+
+            cursor.execute(
+                """
+                INSERT INTO users (first_name, last_name, address, email)
+                VALUES (?, ?, ?, ?)
+            """,
+                (user["first_name"], user["last_name"], user["address"], user["email"]),
+            )
+
+
+if __name__ == "__main__":
+    faker = Faker()
+    faker.seed_instance(SEED)
+    db = sqlite3.connect(FILENAME)
+    try:
+        with db:
+            populate_database(db, faker, COUNT)
+    finally:
+        db.close()
