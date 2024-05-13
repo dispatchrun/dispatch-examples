83b71348953999d38de9d766acd7d747556ba8b9
Chris O'Hara <cohara87@gmail.com>
2024-05-13 13:05:02 +1000

    Update example

--- kafka-consumer/main.py
+++ kafka-consumer/main.py
@@ -1,49 +1,42 @@
 import logging
 import threading
 
+import dispatch
 import requests
-from fastapi import FastAPI
-from dispatch.fastapi import Dispatch
 from kafka import KafkaConsumer
-
-
-# logging.basicConfig(level=logging.DEBUG)
 
 
 BROKERS = ["localhost:9092", "localhost:9093", "localhost:9094"]
 TOPIC = "topic1"
 GROUP = "group1"
 
-URL = "https://c8955422caa67b92b294cef222afe991.m.pipedream.net"
-
-
-app = FastAPI()
-dispatch = Dispatch(app)
-
 
 @dispatch.function
-def send_message(payload):
-    response = requests.post(URL, data=payload)
+def send_message(url, payload):
+    response = requests.post(url, data=payload)
     response.raise_for_status()
     return response
 
 
-def consume_messages():
+def forward_messages(url):
     consumer = KafkaConsumer(TOPIC, group_id=GROUP, bootstrap_servers=BROKERS)
     try:
         for message in consumer:
-            forward_batch([message])
+            send_message.dispatch(url, payload)
     finally:
         consumer.close()
 
 
-def forward_batch(messages):
-    batch = dispatch.batch()
-    for message in messages:
-        batch.add(send_message, message.value)
-    batch.dispatch()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"error: {sys.argv[0]} <URL>")
+        exit(1)
 
+    _, url = sys.argv
 
-# Consume messages in the background.
-thread = threading.Thread(target=consume_messages)
-thread.start()
+    # Start Dispatch in a background thread.
+    thread = threading.Thread(target=dispatch.run)
+    thread.start()
+
+    # Forward messages from Kafka.
+    forward_messages(url)
